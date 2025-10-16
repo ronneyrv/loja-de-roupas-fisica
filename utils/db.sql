1e43a5e888322c1c1b8be888ecc26264044ed126
Ronney Rocha <ronney@email.com>
2025-10-16 18:58:51 -0300

    feat: add doc

---
+++ utils/db.sql
@@ -0,0 +1,45 @@
+-- Tabela de Usuários
+CREATE TABLE Usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    nickname VARCHAR(100) NOT NULL UNIQUE,
+    bio VARCHAR(255),
+    foto VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Tabela de Posts
+CREATE TABLE Post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    foto VARCHAR(255) NOT NULL,
+    legenda VARCHAR(255),
+    localizacao VARCHAR(255),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id) ON DELETE CASCADE
+);
+
+-- Tabela de Comentários
+CREATE TABLE Comentario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    post_id INT,
+    conteudo VARCHAR(255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Post(id) ON DELETE CASCADE
+);
+
+-- Tabela de Curtidas
+CREATE TABLE Curtida (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    post_id INT,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Post(id) ON DELETE CASCADE
+);
